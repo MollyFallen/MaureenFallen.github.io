e6adc1fe7e5ae74823664d5ef656285e7eb01b45
Maureen Fallen <mfallen@uw.edu>
2016-01-25 04:24:56 -0800

    Major Overhaul. Added Javascript. Added fixed scrolling. added dropdown menu. redesigned navbar

---
+++ projects.js
@@ -0,0 +1,35 @@
+$(function(){
+	$(window).scroll(function(event) {
+		var scroll = $(document).scrollTop();
+		if (scroll > 60){
+			$("#HeaderSmall").show();
+			$("#Dropdown").css("position", "fixed");
+			$("#Dropdown").css("top", "60px");
+			$("#Dropdown").css("border-top", "1px solid black");
+		}else{
+			$("#HeaderSmall").hide();
+			$("#Dropdown").css("position", "absolute");
+			$("#Dropdown").css("top", "96px");
+			$("#Dropdown").css("border-top", "none");
+		}
+	});
+	$(".DropdownTrigger").mouseover(function(event) {
+		$("#Dropdown").show();
+	});
+	$("#Dropdown").mouseleave(function(event) {
+		console.log(event.target);
+		if (!(event.relatedTarget.classList.contains("DropdownTrigger"))) {
+			$("#Dropdown").hide();
+		}
+		
+	});
+	$(".DropdownTrigger").mouseleave(function(event) {
+		console.log(event);
+		if (!(event.relatedTarget.id === "Dropdown") && !($.contains($("#Dropdown")[0], event.relatedTarget))) {
+			$("#Dropdown").hide();
+		}
+	});
+
+
+});
+
